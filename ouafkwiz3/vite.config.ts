fbcea31adea0ab37a70a25d17fd50ebfa11e48a1
Felicien <felicien.boury@edu.esiee-it.fr>
2026-04-17 11:41:30 +0200

    fix : bug in vite.config.ts

--- ouafkwiz3/vite.config.ts
+++ ouafkwiz3/vite.config.ts
@@ -1,16 +1,4 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-  }
-})
-
-/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -18,4 +6,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-*/
