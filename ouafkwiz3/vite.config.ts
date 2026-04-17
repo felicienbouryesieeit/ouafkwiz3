f7610c5974471123eb6e570bdf3dbfff1d590499
Felicien <felicien.boury@edu.esiee-it.fr>
2026-04-17 11:38:47 +0200

    fix : bom.txt

--- ouafkwiz3/vite.config.ts
+++ ouafkwiz3/vite.config.ts
@@ -1,3 +1,16 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+  }
+})
+
+/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,3 +18,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+*/
